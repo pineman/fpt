535d693a3c5a6510caa207b6f86166fd86cb7ec4
diogotito <diogotitomarques@gmail.com>
2023-10-02 23:41:27 +0100

    Add winit dependency
    This will make me the #1 contributor in this repo, just like this!

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,45 @@
 use fpt::lr35902::LR35902;
 
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::WindowBuilder,
+};
+
 fn main() {
     let mut lr35902 = LR35902::new();
 
-    loop {
-        lr35902.step();
-    }
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                println!("The close button was pressed; stopping");
+                control_flow.set_exit();
+            }
+            Event::MainEventsCleared => {
+                // Application update code.
+                // lr35902.step();
+
+                // Queue a RedrawRequested event.
+                //
+                // You only need to call this if you've determined that you need to redraw, in
+                // applications which do not always need to. Applications that redraw continuously
+                // can just render here instead.
+                window.request_redraw();
+            }
+            Event::RedrawRequested(_) => {
+                // Redraw the application.
+                //
+                // It's preferable for applications that do not render continuously to render in
+                // this event rather than in MainEventsCleared, since rendering in here allows
+                // the program to gracefully handle redraws requested by the OS.
+            }
+            _ => (),
+        }
+    });
 }
