c0a23c2dd076ef94ec66514ba74a277a90caa3a5
João Freitas <joaj.freitas@gmail.com>
2023-10-02 23:58:34 +0100

    Incomplete set of tests for load 8 bit register

--- tests/lr35902.rs
+++ tests/lr35902.rs
@@ -15,37 +15,79 @@ impl LR35902Builder {
         }
     }
 
-    pub fn with_af(mut self, af: u16) -> LR35902Builder {
+    pub fn with_register(mut self, register: &str, value: u8) -> Self {
+        match register {
+            "b" => self.with_b(value),
+            "c" => self.with_c(value),
+            "d" => self.with_d(value),
+            "e" => self.with_e(value),
+            "h" => self.with_h(value),
+            "l" => self.with_l(value),
+            "a" => self.with_a(value),
+            _ => panic!(),
+        }
+    }
+    pub fn with_af(mut self, af: u16) -> Self {
         self.lr35902.set_af(af);
         self
     }
 
-    pub fn with_a(mut self, a: u8) -> LR35902Builder {
+    pub fn with_b(mut self, b: u8) -> Self {
+        self.lr35902.set_b(b);
+        self
+    }
+
+    pub fn with_c(mut self, c: u8) -> Self {
+        self.lr35902.set_c(c);
+        self
+    }
+
+    pub fn with_d(mut self, d: u8) -> Self {
+        self.lr35902.set_d(d);
+        self
+    }
+
+    pub fn with_e(mut self, e: u8) -> Self {
+        self.lr35902.set_d(e);
+        self
+    }
+
+    pub fn with_h(mut self, h: u8) -> Self {
+        self.lr35902.set_h(h);
+        self
+    }
+
+    pub fn with_l(mut self, l: u8) -> Self {
+        self.lr35902.set_l(l);
+        self
+    }
+
+    pub fn with_a(mut self, a: u8) -> Self {
         self.lr35902.set_a(a);
         self
     }
 
-    pub fn with_bc(mut self, bc: u16) -> LR35902Builder {
+    pub fn with_bc(mut self, bc: u16) -> Self {
         self.lr35902.set_bc(bc);
         self
     }
 
-    pub fn with_de(mut self, de: u16) -> LR35902Builder {
+    pub fn with_de(mut self, de: u16) -> Self {
         self.lr35902.set_de(de);
         self
     }
 
-    pub fn with_hl(mut self, hl: u16) -> LR35902Builder {
+    pub fn with_hl(mut self, hl: u16) -> Self {
         self.lr35902.set_hl(hl);
         self
     }
 
-    pub fn with_sp(mut self, sp: u16) -> LR35902Builder {
+    pub fn with_sp(mut self, sp: u16) -> Self {
         self.lr35902.set_sp(sp);
         self
     }
 
-    pub fn with_pc(mut self, pc: u16) -> LR35902Builder {
+    pub fn with_pc(mut self, pc: u16) -> Self {
         self.lr35902.set_pc(pc);
         self
     }
@@ -88,7 +130,7 @@ fn test_instr_0x000_nop() {
 }
 
 #[rstest]
-#[case(2,1,0x0102)]
+#[case(2, 1, 0x0102)]
 fn test_instr_0x001_ld_bc_d16(#[case] lsb: u8, #[case] msb: u8, #[case] result: u16) {
     // Given
     let builder = LR35902Builder::new()
@@ -110,7 +152,7 @@ fn test_instr_0x001_ld_bc_d16(#[case] lsb: u8, #[case] msb: u8, #[case] result: 
 }
 
 #[rstest]
-#[case(2,1,0x0102)]
+#[case(2, 1, 0x0102)]
 fn test_instr_0x011_ld_de_d16(#[case] lsb: u8, #[case] msb: u8, #[case] result: u16) {
     // Given
     let builder = LR35902Builder::new()
@@ -196,6 +238,73 @@ fn test_instr_0x032_ld_hld_a(#[case] a: u8, #[case] hl: u16) {
         .with_hl(hl - 1) // hl gets decremented
         .with_clock_cycles(8)
         .with_memory_byte(hl, a)
+        .build();
+    assert_eq!(sut, expected);
+}
+
+#[rstest]
+#[case(0x40, "b", "b", 0x01)] //  1 
+#[case(0x40, "b", "b", 0xFF)] //  2 
+#[case(0x41, "b", "c", 0x01)] //  3
+#[case(0x41, "b", "c", 0xFF)] //  4
+#[case(0x42, "b", "d", 0x01)] //  5
+#[case(0x42, "b", "d", 0xFF)] //  6
+//#[case(0x43, "b", "e", 0x01)] //  7 
+//#[case(0x43, "b", "e", 0xFF)] //  8
+#[case(0x44, "b", "h", 0x01)] //  9
+#[case(0x44, "b", "h", 0xFF)] // 10 
+#[case(0x45, "b", "l", 0x01)] // 11
+#[case(0x45, "b", "l", 0xFF)] // 12
+#[case(0x47, "b", "a", 0x01)] // 13
+#[case(0x47, "b", "a", 0xFF)] // 14
+#[case(0x48, "c", "b", 0x01)] // 13
+#[case(0x48, "c", "b", 0xFF)] // 14
+#[case(0x49, "c", "c", 0x01)] // 15 
+#[case(0x49, "c", "c", 0xFF)] // 16 
+#[case(0x4A, "c", "d", 0x01)] // 17
+#[case(0x4A, "c", "d", 0xFF)] // 18
+//#[case(0x4B, "c", "e", 0x01)] // 19
+//#[case(0x4B, "c", "e", 0xFF)] // 20
+#[case(0x4C, "c", "h", 0x01)] // 21
+#[case(0x4C, "c", "h", 0xFF)] // 22 
+#[case(0x4D, "c", "l", 0x01)] // 23
+#[case(0x4D, "c", "l", 0xFF)] // 24
+#[case(0x4F, "c", "a", 0x01)] // 25
+#[case(0x4F, "c", "a", 0xFF)] // 26
+#[case(0x50, "d", "b", 0x01)] // 27
+#[case(0x50, "d", "b", 0xFF)] // 28
+//#[case(0x51, "d", "c", 0x01)] // 29 
+//#[case(0x51, "d", "c", 0xFF)] // 30 
+//#[case(0x52, "d", "d", 0x01)] // 31
+//#[case(0x52, "d", "d", 0xFF)] // 32
+//#[case(0x53, "d", "e", 0x01)] // 33
+//#[case(0x53, "d", "e", 0xFF)] // 34
+#[case(0x54, "d", "h", 0x01)] // 35
+#[case(0x54, "d", "h", 0xFF)] // 36 
+#[case(0x55, "d", "l", 0x01)] // 37
+#[case(0x55, "d", "l", 0xFF)] // 38
+#[case(0x57, "d", "a", 0x01)] // 39
+#[case(0x57, "d", "a", 0xFF)] // 40
+fn test_load_8_bit_reg_to_8_bit_reg(
+    #[case] instr: u8,
+    #[case] dst_reg: &str,
+    #[case] src_reg: &str,
+    #[case] value: u8,
+) {
+    // Given
+    let builder = LR35902Builder::new()
+        .with_memory_byte(0x0000, instr) // instruction LD (HL-), a
+        .with_register(src_reg, value);
+    let mut sut = builder.clone().build();
+
+    // When
+    sut.step();
+
+    // Then
+    let expected = builder
+        .with_pc(1)
+        .with_clock_cycles(4)
+        .with_register(dst_reg, value) // hl gets decremented
         .build();
     assert_eq!(sut, expected);
 }
