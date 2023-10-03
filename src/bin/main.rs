c233dd462141c5aa793276edf6eb7e06ae6fe3cb
João Freitas <joaj.freitas@gmail.com>
2023-10-03 19:26:16 +0100

    Split gui and main binaries

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -11,46 +11,9 @@ use winit::{
 fn main() {
     let mut lr = LR35902::new();
 
-    let the_thing = thread::spawn(move || loop {
+    loop {
         lr.step();
-    });
+    }
 
-    the_loop();
-    the_thing.join().unwrap();
 }
 
-fn the_loop() {
-    let event_loop: EventLoop<()> = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-
-    event_loop.run(move |event, _, control_flow| {
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => {
-                println!("The close button was pressed; stopping");
-                control_flow.set_exit();
-            }
-            Event::MainEventsCleared => {
-                // Application update code.
-                // lr35902.step();
-
-                // Queue a RedrawRequested event.
-                //
-                // You only need to call this if you've determined that you need to redraw, in
-                // applications which do not always need to. Applications that redraw continuously
-                // can just render here instead.
-                window.request_redraw();
-            }
-            Event::RedrawRequested(_) => {
-                // Redraw the application.
-                //
-                // It's preferable for applications that do not render continuously to render in
-                // this event rather than in MainEventsCleared, since rendering in here allows
-                // the program to gracefully handle redraws requested by the OS.
-            }
-            _ => (),
-        }
-    });
-}
