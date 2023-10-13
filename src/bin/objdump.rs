ef501324f16d1d29922eba87b628f3be90760c6a
joajfreitas <joaj.freitas@gmail.com>
2023-10-13 19:19:25 +0100

    Remove dbg

--- src/bin/objdump.rs
+++ src/bin/objdump.rs
@@ -2,11 +2,11 @@ use fpt::lr35902::LR35902;
 
 use clap::Parser;
 
-/// Simple program to greet a person
+/// Parse rom and output the disassembly
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
+    /// Flag to enable debug output
     #[arg(short, long)]
     debug: bool,
 }
