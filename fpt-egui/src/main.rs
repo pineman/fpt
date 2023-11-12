e1de74f6f097b6913cd60c5112b7aec6fa4aec3b
João Pinheiro <joao.castropinheiro@gmail.com>
2023-11-12 20:32:39 +0000

    tweak

--- fpt-egui/src/main.rs
+++ fpt-egui/src/main.rs
@@ -1,4 +1,7 @@
-use egui::{Color32, Rect, Vec2, Widget};
+use std::time::{Duration, Instant};
+
+use egui::Color32;
+use log::info;
 use sha2::Digest;
 
 const GB_FRAME_IN_SECONDS: f64 = 0.016666666667;
@@ -33,15 +36,20 @@ impl TemplateApp {
     }
 }
 
-// #[cfg(target_arch = "wasm32")]
-// fn now() -> f64 {
-//     use wasm_bindgen::JsCast;
-//     use wasm_bindgen::JsValue;
-//     js_sys::Reflect::get(&js_sys::global(), &JsValue::from_str("performance"))
-//         .expect("failed to get performance from global object")
-//         .unchecked_into::<web_sys::Performance>()
-//         .now()
-// }
+#[cfg(target_arch = "wasm32")]
+fn now() -> f64 {
+    use wasm_bindgen::JsCast;
+    use wasm_bindgen::JsValue;
+    js_sys::Reflect::get(&js_sys::global(), &JsValue::from_str("performance"))
+        .expect("failed to get performance from global object")
+        .unchecked_into::<web_sys::Performance>()
+        .now()
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn now() -> Instant {
+    Instant::now()
+}
 
 // fn calc_sha256(input: &str) -> String {
 //     let mut hasher = sha2::Sha256::new();
@@ -66,11 +74,14 @@ impl eframe::App for TemplateApp {
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
+            let a = now();
             let time = ui.input(|i| i.time);
             let delta_time = ui.input(|i| i.unstable_dt) as f64;
             self.accum_time += delta_time;
-            // let gb_frame;
+            let mut image = Default::default();
+            let before = self.gb_frame_count;
             while self.accum_time >= GB_FRAME_IN_SECONDS {
+                // if self.accum_time >= GB_FRAME_IN_SECONDS {
                 self.gb_frame_count += 1;
                 // ... RENDER GAME BOY SCREEN ...
                 // uncomment to make Tito's fans slightly noisier
@@ -79,10 +90,25 @@ impl eframe::App for TemplateApp {
                 // }
                 // gb_frame = gb.get_frame();
                 self.accum_time -= GB_FRAME_IN_SECONDS;
+                image = egui::ColorImage::new([128, 64], Color32::RED);
+                self.texture = None;
+                for i in 0..(((self.gb_frame_count as usize) * 10) % (128 * 64)) {
+                    image.pixels[i] = Color32::YELLOW;
+                }
             }
+            let texture: &egui::TextureHandle = self.texture.get_or_insert_with(|| {
+                // Load the texture only once.
+                ui.ctx().load_texture("my-image", image, Default::default())
+            });
+            ui.image((texture.id(), texture.size_vec2()));
             // ui.load_texture(gb_frame);
             self.last_time = time;
 
+            let mut ccc = false;
+            if self.gb_frame_count - before > 1 {
+                info!("more than one gb_frame");
+                ccc = true;
+            }
             egui::Grid::new("my_grid").striped(true).show(ui, |ui| {
                 macro_rules! stat {
                     ($label:literal : $value:expr) => {
@@ -100,22 +126,33 @@ impl eframe::App for TemplateApp {
                 stat!("Frame count" : format!("{}"   , self.gb_frame_count));
                 stat!("UI updates"  : format!("{}"   , self.egui_frame_count));
             });
-
-            ui.separator();
-
-            ui.heading("fpt");
             self.egui_frame_count += 1;
+            ui.heading("fpt");
             ui.add(egui::Label::new(self.egui_frame_count.to_string()));
 
-            let mut image = egui::ColorImage::new([128, 64], Color32::RED);
-            for i in 0..(((self.egui_frame_count as usize) * 10) % (128 * 64)) {
-                image.pixels[i] = Color32::YELLOW;
-            }
-            let texture = ui.ctx().load_texture("my-image", image, Default::default());
-            ui.image((texture.id(), texture.size_vec2()));
-        });
+            let b = now();
+            info!("a {:.8}", a);
+            info!("b {:.8}", b);
+            let time_taken = (b - a) / 1000.0;
+            info!("time_taken {:.8}", time_taken);
 
-        ctx.request_repaint();
+            // let time_taken = a.elapsed().as_secs_f64();
+            // if ccc {
+            //     info!("time_taken1 {:.8}", time_taken);
+            // }
+            // let time_taken = (time_taken * 1000.0).floor() / 1000.0;
+            // if ccc {
+            //     info!("time_taken2 {:.8}", time_taken);
+            // }
+
+            let sleep_time = GB_FRAME_IN_SECONDS - time_taken;
+            info!("sleep_time {:.8}", sleep_time);
+            if sleep_time < 0.0 {
+                ctx.request_repaint();
+            } else {
+                ctx.request_repaint_after(Duration::from_secs_f64(sleep_time));
+            }
+        });
     }
 }
 
