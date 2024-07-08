214183681f4a3134bb11bf4bc15b7f2f31d24c10
João Freitas <joaj.freitas@gmail.com>
2024-07-08 22:21:01 +0100

    Fetch test roms from build.rs

--- fpt/build.rs
+++ fpt/build.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
+use std::process::Command;
 
 use serde::{Deserialize, Serialize};
 
@@ -19,10 +20,9 @@ struct Test {
     enabled: Option<bool>,
 }
 
-// build script's entry point
-fn main() {
+fn generate_rom_tests() {
     let out_dir = env::var("OUT_DIR").unwrap();
-    let destination = dbg!(Path::new(&out_dir).join("tests.rs"));
+    let destination = Path::new(&out_dir).join("tests.rs");
     let mut test_file = File::create(destination).unwrap();
 
     write_header(&mut test_file);
@@ -30,7 +30,7 @@ fn main() {
 }
 
 fn write_test(test_file: &mut File, directory: &str) {
-    let source = dbg!(std::fs::read_to_string(directory).unwrap());
+    let source = std::fs::read_to_string(directory).unwrap();
     let suite: Suite = serde_json::from_str(&source).unwrap();
 
     for test in suite.tests {
@@ -52,4 +52,27 @@ fn write_test(test_file: &mut File, directory: &str) {
 
 fn write_header(test_file: &mut File) {
     write!(test_file, include_str!("./tests/templates/header")).unwrap();
+}
+
+fn run_cmd(cmd: &str) -> String {
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(cmd)
+        .output()
+        .expect("failed to execute process");
+
+    String::from_utf8(output.stdout).expect("Failed to convert output to string")
+}
+
+fn fetch_mooneye_test_roms() {
+    run_cmd("wget https://gekkio.fi/files/mooneye-test-suite/mts-20240127-1204-74ae166/mts-20240127-1204-74ae166.tar.xz");
+    run_cmd("tar -xvf mts-20240127-1204-74ae166.tar.xz");
+    run_cmd("mkdir -p third_party/mooneye-test-suite");
+    run_cmd("mv mts-20240127-1204-74ae166 third_party/mooneye-test-suite/build");
+    run_cmd("tree .");
+}
+
+fn main() {
+    generate_rom_tests();
+    fetch_mooneye_test_roms();
 }
