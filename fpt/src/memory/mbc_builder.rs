443990619c2e7b79ed38240ff0a5f0eff54c6b74
João Freitas <joaj.freitas@gmail.com>
2024-07-25 23:35:50 +0100

    Document mbc builder

--- fpt/src/memory/mbc_builder.rs
+++ fpt/src/memory/mbc_builder.rs
@@ -8,8 +8,9 @@ use super::Cartridge;
 pub fn create_mbc(cartridge_data: &[u8]) -> Rc<RefCell<dyn Cartridge>> {
     let cartridge_type = get_cartridge_type(cartridge_data);
 
+    // https://gbdev.io/pandocs/The_Cartridge_Header.html#0147--cartridge-type
     match cartridge_type {
-        0x00 => Rc::new(RefCell::new(NoMbcCartridge::new(cartridge_data))),
+        0x00 => Rc::new(RefCell::new(NoMbcCartridge::new(cartridge_data))), // rom only
         _ => panic!(),
     }
 }
