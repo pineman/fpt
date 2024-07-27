2c39c85f731578ed4e521c56163cbd3d650c6a18
João Freitas <joaj.freitas@gmail.com>
2024-07-27 15:12:27 +0100

    Define constants for memory ranges and bank sizes

--- fpt/src/memory/mbc3.rs
+++ fpt/src/memory/mbc3.rs
@@ -11,6 +11,13 @@ pub struct Mbc3Cartridge {
     ram_bank_number: usize,
 }
 
+const ROM_BANK_SIZE: usize = 0x4000;
+const RAM_BANK_SIZE: usize = 0x2000;
+const RAM_ENABLE: MemoryRange = 0x0000..0x2000;
+const ROM_BANK_NUMBER: MemoryRange = 0x2000..0x4000;
+const RAM_BANK_NUMBER: MemoryRange = 0x4000..0x6000;
+//const LATCH_CLOCK_DATA: MemoryRange = 0x6000..0x8000;
+
 impl Mbc3Cartridge {
     pub fn new(cartridge: &[u8]) -> Mbc3Cartridge {
         let rom_size = convert_rom_size(get_rom_size(cartridge));
@@ -57,9 +64,9 @@ impl Cartridge for Mbc3Cartridge {
         }
     }
     fn write(&mut self, address: Address, value: u8) {
-        if (0x0000..0x2000).contains(&address) {
+        if RAM_ENABLE.contains(&address) {
             self.ext_ram_enabled = value & 0xF == 0xA;
-        } else if (0x2000..0x4000).contains(&address) {
+        } else if ROM_BANK_NUMBER.contains(&address) {
             // TODO: rom bank number upper bits
             let rom_bank_number = value & 0x1F;
             if rom_bank_number == 0 {
@@ -67,7 +74,7 @@ impl Cartridge for Mbc3Cartridge {
             } else {
                 self.rom_bank_number = rom_bank_number as usize; // TODO: needs to be masked to log2(#banks)
             }
-        } else if (0x4000..0x6000).contains(&address) {
+        } else if RAM_BANK_NUMBER.contains(&address) {
             let ram_bank_number = value & 0x3;
             self.ram_bank_number = ram_bank_number as usize; // TODO: needs to be checked for number of ram
                                                              // banks
